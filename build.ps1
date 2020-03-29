40816411a165a8f5e0565300e2dae68258782bff
Erwin Werkman <werkman.erwin@gmail.com>
2020-03-29 13:44:04 +0200

    Working on build...

--- build.ps1
+++ build.ps1
@@ -1,1 +1,20 @@
+$Settings = @{
+    ManifestPath = '{0}\{1}\{1}.psd1' -f $PSScriptRoot, 'Pesci'
+    Version = $env:APPVEYOR_BUILD_VERSION
+    VersionRegex = "ModuleVersion\s=\s'(?<ModuleVersion>\S+)'" -as [regex]
+}
+
+$ManifestContent = Get-Content -Path $Settings.ManifestPath
+$CurrentVersion = $Settings.VersionRegex.Match($ManifestContent).Groups['ModuleVersion'].Value
+"Current module version in the manifest : $CurrentVersion"
+
+$ManifestContent -replace $CurrentVersion,$Settings.Version | Set-Content -Path $Settings.ManifestPath -Force
+$NewManifestContent = Get-Content -Path $Settings.ManifestPath
+$NewVersion = $Settings.VersionRegex.Match($NewManifestContent).Groups['ModuleVersion'].Value
+"Updated module version in the manifest : $NewVersion"
+
+If ( $NewVersion -ne $Settings.Version ) {
+    Throw "Module version was not updated correctly to $($Settings.Version) in the manifest."
+}
+
 Publish-Module -Path Pesci -NuGetApiKey $ENV:NugetApiKey -Verbose -WhatIf
